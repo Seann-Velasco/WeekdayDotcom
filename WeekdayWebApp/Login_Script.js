57e3951eda648d9710469eeb54311fe20b35b15f
Seann Velasco <velasco.317021@marikina.sti.edu.ph>
2025-02-26 04:37:49 +0800

    Delete WeekdayWebApp directory

--- WeekdayWebApp/Login_Script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('nav');
-    if (window.scrollY > 5) {
-        navbar.classList.add('solid');
-    } else {
-        navbar.classList.remove('solid');
-    }
-});
